7809230e810851f12fb1e9a98c457c300a12fd1e
Roombie <120424080+Roombie@users.noreply.github.com>
2025-08-12 02:30:16 -0400

    add run skins

--- Assets/Scripts/Gameplay/Ghost.cs
+++ Assets/Scripts/Gameplay/Ghost.cs
@@ -2,7 +2,26 @@ using UnityEngine;
 
 public class Ghost : MonoBehaviour
 {
+    public Movement movement;
     private Pacman pacman;
+    public enum GhostType
+    {
+        Blinky,
+        Inky,
+        Pinky,
+        Clyde
+    }
+
+    public enum Mode
+    {
+        Home,
+        Scatter,
+        Chase,
+        Frightened,
+        Eaten
+    }
+
+    Mode currentMode = Mode.Home;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
